d64bdde2d069ec8a3dbef0a04322731eff339c92
Jacob Benson <benson.b.jacob@gmail.com>
2023-02-18 08:34:41 -0600

    Add Preview mode and BlogList

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,5 +1,36 @@
-function HomePage() {
-  return <div>app</div>;
-}
+import { previewData } from 'next/headers';
+import { groq } from 'next-sanity';
+import { client } from '../../lib/sanity.client';
+import { PreviewSuspense } from 'next-sanity/preview';
+import PreviewBlogList from '../../components/PreviewBlogList';
+import BlogList from '../../components/BlogList';
 
-export default HomePage;
+const query = groq`
+  *[_type=='post'] {
+    ...,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
+`;
+
+export default async function page() {
+  if (previewData()) {
+    return (
+      <PreviewSuspense
+        fallback={
+          <div role='status'>
+            <p className='text-center text-lg animate-pulse'>
+              Loading Preview Data...
+            </p>
+          </div>
+        }
+      >
+        <PreviewBlogList query={query} />
+      </PreviewSuspense>
+    );
+  }
+
+  const posts = await client.fetch(query);
+
+  return <BlogList posts={posts} />;
+}
